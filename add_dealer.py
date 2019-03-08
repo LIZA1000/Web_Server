20e650c02af20d4a8c237c20aefdca05919b33d8
Andrew Rogov <andrewrogov@mail.ru>
2019-03-08 22:23:04 +0500

    Delete some files

--- add_dealer.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, IntegerField
-from wtforms.validators import DataRequired
-
-
-class AddDealerForm(FlaskForm):
-    name = StringField('Название центра', validators=[DataRequired()])
-    address = StringField('Адрес', validators=[DataRequired()])
-    submit = SubmitField('Добавить')
